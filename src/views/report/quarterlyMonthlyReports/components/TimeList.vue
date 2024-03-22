e9c9a70e709d514c85d6850cf7ac2034c0d898c6
yanyahong <yanyahong@eesaenergy.com>
2024-03-22 16:52:50 +0800

    feat: init report

---
+++ src/views/report/quarterlyMonthlyReports/components/TimeList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="ns.bm('list', 'item')">
+        <div :class="ns.bm('item', 'header')">
+            <p>储能周报</p>
+        </div>
+        <div :class="ns.bm('item', 'tip')">
+            <p>以周为维度汇总储能招标/中标项目，每周三更新。</p>
+        </div>
+        <div :class="ns.bm('item', 'body')">
+            <p>以周为维度汇总储能招标/中标项目，每周三更新。</p>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+import { onMounted, ref, watch } from 'vue'
+import useNamespace from '@/utils/nameSpace'
+const ns = useNamespace('timeList')
+</script>
+
+<style lang="scss">
+@import "@/style/mixin.scss";
+</style>
---
+++ src/views/report/quarterlyMonthlyReports/index.vue
@@ -0,0 +1,39 @@
+<template>
+     <div :class="[ns.b(), 'es-commonPage']">
+        <div :class="ns.b('main')">
+            <div :class="ns.bm('main', 'title')">月报季报。</div>
+            <div :class="ns.b('list')">
+                <TimeList />
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script lang="ts" setup>
+import { onMounted, ref, watch } from 'vue'
+import useNamespace from '@/utils/nameSpace'
+import TimeList from './components/TimeList.vue'
+const ns = useNamespace('quarterlyMonthly')
+</script>
+
+<style lang="scss">
+@import "@/style/mixin.scss";
+
+.es-quarterlyMonthly {
+    // height: calc(50vh - 56px);
+    @include margin(56px, auto, 0, auto);
+}
+.es-quarterlyMonthly-main {
+    max-width: 1152px;
+    @include margin(0, auto, 0, auto);
+    @include padding(80px, 0, 0, 0);
+    @media (max-width: 1250px) {
+        width: 96%;
+        margin: 0 auto;
+    }
+    .es-quarterlyMonthly-main--title {
+        @include font(36px, 600, rgba(0, 0, 0, 0.9), 44px);
+    }
+}
+</style>
