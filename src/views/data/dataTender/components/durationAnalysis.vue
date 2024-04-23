745bb449fb045e9f7ca1f98a4cbbefe5401083f9
BigGang <chg@eesaenergy.com>
2024-04-23 17:40:55 +0800

    feat: durationAnalysis

---
+++ src/views/data/dataTender/components/durationAnalysis.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="duration-analysis">
+    <div class="filter">
+      <Select
+        title="招标内容"
+        :options="contentList"
+        valueKey="regionName"
+        :defaultValue="searchParams.c1"
+        @triggerForm="handleTriggerForm"
+        @on-change="(val) => handleChange(val, 'c1')"
+      />
+      <Select
+        title="发布日期"
+        :options="dateList"
+        valueKey="paramName"
+        labelKey="paramDesc"
+        :defaultValue="searchParams.c2"
+        @triggerForm="handleTriggerForm"
+        @on-change="(val) => handleChange(val, 'c2')"
+      />
+      <Select
+        title="招标地区"
+        :options="regionList"
+        valueKey="paramName"
+        labelKey="paramDesc"
+        :defaultValue="searchParams.c3"
+        @triggerForm="handleTriggerForm"
+        @on-change="(val) => handleChange(val, 'c3')"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Select from "@/components/Common/Select.vue";
+import { ref } from "vue";
+const contentList = ref([]);
+const dateList = ref([]);
+const regionList = ref([]);
+const searchParams = ref({ c1: "", c2: "", c3: "" });
+
+// 用户触发表单
+function handleTriggerForm() {}
+
+// 筛选项发生变化时
+function handleChange(val, key) {
+  searchParams.value[key] = val;
+}
+</script>
+
+<style lang="scss" scoped>
+.duration-analysis {
+  margin-top: 9px;
+  .filter {
+    display: flex;
+  }
+}
+</style>
--- src/views/data/dataTender/enter.vue
+++ src/views/data/dataTender/enter.vue
@@ -23,6 +23,7 @@
         :contentFilter="contentFilter"
         :timeFilter="timeFilter"
       />
+      <DurationAnalysis v-if="choseTabs === 6" />
     </template>
   </div>
 </template>
@@ -32,24 +33,25 @@ interface TabsList {
   id: number;
   name: string;
 }
+import {
+  getTenderFilterApi,
+  getBiddingContentApi,
+  getTenderTimeFilterApi,
+} from "@/api/data";
 import { ref, Ref } from "vue";
 import useNamespace from "@/utils/nameSpace";
 import TenderSearch from "./components/tenderSearch.vue";
+import AreaAnalysis from "./components/areaAnalysis.vue";
 import MonthlyAnalysis from "./components/monthlyAnalysis.vue";
 import BusinessAnalysis from "./components/businessAnalysis.vue";
-import AreaAnalysis from "./components/areaAnalysis.vue";
-import {
-  getTenderFilterApi,
-  getTenderTimeFilterApi,
-  getBiddingContentApi,
-} from "@/api/data";
+import DurationAnalysis from "./components/durationAnalysis.vue";
 import { NOOP } from "@vue/shared";
 import { windowScrollStore } from "@/store/modules/windowScroll";
-const windowScroll = windowScrollStore();
 const ns = useNamespace("dataTender");
-const choseTabs: Ref<number> = ref(1); // 选中的标签栏
-const contentFilter: Ref<Array<any>> = ref([]); // 招标内容筛选项
+const windowScroll = windowScrollStore();
+const choseTabs: Ref<number> = ref(6); // 选中的标签栏
 const timeFilter: Ref<Array<any>> = ref([]); // 招标时间筛选项
+const contentFilter: Ref<Array<any>> = ref([]); // 招标内容筛选项
 windowScroll.SET_SCROLL_TOP(0);
 
 const biddingContentFilter: Ref<Array<any>> = ref([
@@ -68,6 +70,7 @@ const tabsList: Ref<Array<TabsList>> = ref([
   { id: 2, name: "招标月度分析" },
   { id: 3, name: "招标企业分析" },
   { id: 4, name: "招标地区分析" },
+  { id: 6, name: "储能时长分析" },
 ]);
 // 选择标签栏
 const onHandleClick = (id: number) => {
