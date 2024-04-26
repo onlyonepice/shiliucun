f98eac0c1ab4257b3579df91c91400e25ac5418e
gws <guoweisheng@eesaenergy.com>
2024-04-26 10:37:43 +0800

    feat: add search product

--- src/router/modules/enterprise.ts
+++ src/router/modules/enterprise.ts
@@ -1,10 +1,19 @@
 export default {
   path: "/enterprise",
   name: "Enterprise",
-  component: {
-    default: () => import("@/views/enterprise/index.vue"),
-  },
   meta: {
     title: "企业",
   },
+  children: [
+    {
+      path: "/searchProduct",
+      name: "SearchProduct",
+      components: {
+        default: () => import("@/views/searchProduct/index.vue"),
+      },
+      meta: {
+        title: "查产品",
+      },
+    },
+  ],
 };
---
+++ src/views/searchProduct/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <p :class="[ns.b(), 'es-commonPage']">查产品</p>
+</template>
+
+<script lang="ts" setup>
+import useNamespace from "@/utils/nameSpace";
+const ns = useNamespace("searchProduct");
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/mixin.scss";
+.home {
+  height: calc(50vh - 56px);
+}
+</style>
