66529e29686d2acb347ba6abd9277f0c81c9fb81
BigGang <chg@eesaenergy.com>
2024-05-09 14:25:30 +0800

    feat: online-report-skeleton

--- src/views/report/expertInterviews/enter.vue
+++ src/views/report/expertInterviews/enter.vue
@@ -1,18 +1,6 @@
 <template>
   <div :class="[ns.b(), 'es-commonPage']">
-    <div v-if="skeletonScreen" class="skeleton-screen">
-      <div class="title-box" />
-      <div class="list-box">
-        <div v-for="i in 10" :key="i" class="item-box">
-          <div class="item-top-box" />
-          <div class="item-bottom-box">
-            <div class="item-bottom-left-1-box" />
-            <div class="item-bottom-left-2-box" />
-            <div class="item-bottom-right-2-box" />
-          </div>
-        </div>
-      </div>
-    </div>
+    <Skeleton v-if="skeletonScreen" />
     <template v-else>
       <div :class="ns.b('title')">专家访谈</div>
       <div :class="ns.b('content')">
@@ -75,6 +63,7 @@
 </template>
 <script lang="ts" setup>
 import { ref, computed } from "vue";
+import Skeleton from "./skeleton.vue";
 import { useRouter } from "vue-router";
 import { getToken } from "@/utils/auth";
 import useNamespace from "@/utils/nameSpace";
@@ -137,53 +126,6 @@ getExpertInterviews();
 
 .es-expertInterviews {
   padding: 80px 24px;
-
-  .skeleton-screen {
-    .title-box {
-      @include widthAndHeight(160px, 44px);
-      @include box(0, 0, #f2f3f5, 8px);
-    }
-
-    .list-box {
-      margin-top: 32px;
-
-      .item-box {
-        margin-top: 40px;
-
-        &:first-child {
-          margin-top: 0;
-        }
-
-        .item-top-box {
-          @include widthAndHeight(100%, 24px);
-          @include box(0, 0, #f2f3f5, 4px);
-        }
-
-        .item-bottom-box {
-          margin-top: 12px;
-          @include flex(center, flex-start);
-          .item-bottom-right-2-box,
-          .item-bottom-left-1-box,
-          .item-bottom-left-2-box {
-            @include box(0, 0, #f2f3f5, 4px);
-          }
-          .item-bottom-left-1-box {
-            @include widthAndHeight(45px, 24px);
-          }
-
-          .item-bottom-left-2-box {
-            @include widthAndHeight(48px, 24px);
-            margin-left: 8px;
-            margin-right: auto;
-          }
-
-          .item-bottom-right-2-box {
-            @include widthAndHeight(131px, 24px);
-          }
-        }
-      }
-    }
-  }
 
   .es-expertInterviews-title {
     @include font(36px, 600, rgba(0, 0, 0, 0.9), 44px);
---
+++ src/views/report/expertInterviews/skeleton.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="skeleton-screen">
+    <div class="title-box" />
+    <div class="list-box">
+      <div v-for="i in 10" :key="i" class="item-box">
+        <div class="item-top-box" />
+        <div class="item-bottom-box">
+          <div class="item-bottom-left-1-box" />
+          <div class="item-bottom-left-2-box" />
+          <div class="item-bottom-right-2-box" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "@/style";
+.skeleton-screen {
+  .title-box {
+    @include widthAndHeight(160px, 44px);
+    @include box(0, 0, #f2f3f5, 8px);
+  }
+
+  .list-box {
+    margin-top: 32px;
+
+    .item-box {
+      margin-top: 40px;
+
+      &:first-child {
+        margin-top: 0;
+      }
+
+      .item-top-box {
+        @include widthAndHeight(100%, 24px);
+        @include box(0, 0, #f2f3f5, 4px);
+      }
+
+      .item-bottom-box {
+        margin-top: 12px;
+        @include flex(center, flex-start);
+        .item-bottom-right-2-box,
+        .item-bottom-left-1-box,
+        .item-bottom-left-2-box {
+          @include box(0, 0, #f2f3f5, 4px);
+        }
+        .item-bottom-left-1-box {
+          @include widthAndHeight(45px, 24px);
+        }
+
+        .item-bottom-left-2-box {
+          @include widthAndHeight(48px, 24px);
+          margin-left: 8px;
+          margin-right: auto;
+        }
+
+        .item-bottom-right-2-box {
+          @include widthAndHeight(131px, 24px);
+        }
+      }
+    }
+  }
+}
+</style>
