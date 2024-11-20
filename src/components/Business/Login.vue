08bd4649a4b04256bb2333501c5c2255ecc4b70e
guoweisheng <2193185903@qq.com>
2024-11-20 19:56:15 +0800

    完成静态页面

--- src/components/Business/Login.vue
+++ src/components/Business/Login.vue
@@ -1,12 +1,55 @@
 <template>
   <el-dialog
     v-model="dialogVisible"
-    title="欢迎来到 {{ openLoginType }} 界面"
+    title="欢迎来到十六村"
     width="500"
     :before-close="handleClose"
     class="login-dialog"
   >
-    <span>This is a message</span>
+    <template v-if="useUserStoreHook().$state.openLoginType === 'login'">
+      <el-input
+        v-model="form.email"
+        style="width: 100%; margin-bottom: 1.25vw"
+        placeholder="请输入邮箱地址"
+      />
+      <el-input
+        v-model="form.password"
+        style="width: 100%; margin-bottom: 1.25vw"
+        placeholder="请输入密码"
+        type="password"
+        :show-password="!showPassword"
+      />
+      <p class="passwordTips___uSglk">忘记密码？</p>
+    </template>
+    <template v-else>
+      <el-input
+        v-model="form.email"
+        style="width: 100%; margin-bottom: 1.25vw"
+        placeholder="请输入邮箱地址"
+      />
+      <div class="registerCode">
+        <el-input
+          v-model="form.code"
+          style="width: 64%"
+          placeholder="请输入验证码"
+        />
+        <div class="registerCode-btn" @click="onSendCode">{{ btnDesc }}</div>
+      </div>
+      <el-input
+        v-model="form.password"
+        style="width: 100%; margin-bottom: 1.25vw"
+        placeholder="请输入密码"
+        type="password"
+        :show-password="!showPassword"
+      />
+      <el-input
+        v-model="form.password"
+        style="width: 100%; margin-bottom: 1.25vw"
+        placeholder="请输入密码"
+        type="password"
+        :show-password="!showPassword"
+      />
+    </template>
     <template #footer>
       <div class="dialog-footer">
         <el-button @click="dialogVisible = false" class="btn-foot">{{
@@ -17,10 +60,14 @@
   </el-dialog>
 </template>
 <script lang="ts" setup>
-import { ref, watch } from "vue";
+import { ref, watch, Ref } from "vue";
 import { useUserStoreHook } from "@/store/modules/user";
+import { ElMessage } from "element-plus";
 const dialogVisible = ref(true);
 const openLoginType = ref(""); // 登录方式
+const form: Ref<any> = ref({});
+const showPassword: Ref<boolean> = ref(false); // 密码是否可见
+const btnDesc: Ref<string> = ref("获取验证码"); // 倒计时文案
 watch(
   () => useUserStoreHook().$state.openLoginType,
   (newVal) => {
@@ -33,6 +80,36 @@ watch(
 const handleClose = () => {
   useUserStoreHook().openLogin(false, useUserStoreHook().$state.openLoginType);
 };
+// 倒计时
+const timer: Ref<any> = ref(null); // 定时器
+const countDown = () => {
+  btnDesc.value = "60s 后可重发";
+  timer.value = setInterval(() => {
+    let seconds = Number(btnDesc.value.replace("s 后可重发", ""));
+    seconds--;
+    btnDesc.value = seconds + "s 后可重发";
+    if (seconds === 0) {
+      clearInterval(timer.value);
+      btnDesc.value = "重新获取";
+    }
+  }, 1000);
+};
+// 发送验证码
+const onSendCode = async () => {
+  try {
+    if (btnDesc.value.indexOf("s") !== -1) {
+      return false;
+    }
+    if (form.value.email === "") {
+      return ElMessage.error("请输入邮箱");
+    }
+    // await sendCode({ mobile: loginForm.value.mobile });
+    ElMessage.success("发送成功");
+    countDown();
+  } catch (error) {
+    console.log(error);
+  }
+};
 </script>
 
 <style lang="scss">
@@ -43,6 +120,30 @@ const handleClose = () => {
   .btn-foot {
     width: 100%;
     @include flex(center, center, nowrap);
+  }
+}
+.passwordTips___uSglk {
+  font-size: 0.72917vw;
+  font-weight: 500;
+  color: #b6b3b3;
+}
+.registerCode {
+  @include flex(center, space-between, nowrap);
+  margin-bottom: 1.25vw;
+  height: 32px;
+  .registerCode-btn {
+    width: 32%;
+    background-color: #323030;
+    color: #b6b3b3;
+    font-size: 0.83333vw;
+    font-weight: 500;
+    padding: 0.625vw;
+    border-radius: 0.625vw;
+    text-align: center;
+    cursor: pointer;
+    &:hover {
+      background-color: #222121;
+    }
   }
 }
 </style>
--- src/components/Business/PageHead.vue
+++ src/components/Business/PageHead.vue
@@ -3,13 +3,17 @@
     <div class="pageHead-content">
       <img :src="Logo" alt="" />
       <div>
-        <el-button>立即游玩</el-button>
+        <el-button @click="openDialog" class="btn-play">立即游玩</el-button>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
 import Logo from "@/assets/img/logo.png";
+import { useUserStoreHook } from "@/store/modules/user";
+const openDialog = () => {
+  useUserStoreHook().openLogin(true, "login");
+};
 </script>
 
 <style lang="scss">
@@ -28,5 +32,11 @@ import Logo from "@/assets/img/logo.png";
       height: 1.66667vw;
     }
   }
+}
+.btn-play {
+  height: 2.08333vw;
+  font-size: 0.72917vw;
+  line-height: 1.04167vw;
+  padding: 1.04167vw !important;
 }
 </style>
